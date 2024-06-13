ed2c4b4e8531f4337071c08143dc52e91cff8f53
Oleksii Korniienko <oleksii.korniienko@idealscorp.com>
2024-06-13 13:01:07 +0300

    Added routing and additional components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {CrisisListComponent} from "./crisis-list/crisis-list.component";
+import {HeroesListComponent} from "./heroes-list/heroes-list.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'crisis', component: CrisisListComponent},
+  {path: 'heroes', component: HeroesListComponent},
+  { path: '**', component: NotFoundComponent },
+];
